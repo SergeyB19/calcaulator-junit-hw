9a82e2ab82ccc7545b3ec30e0c3d156f2d8372df
my.h.m@yandex.ru <Zxcv7788jetbrains>
2022-08-22 18:08:30 +0300

    Initial commit

---
+++ src/test/java/pro/sky/java/course2/calcaulatorjunithw/CalcaulatorJunitHwApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.java.course2.calcaulatorjunithw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalcaulatorJunitHwApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
